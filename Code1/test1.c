08b3afb55d6df5711b98a51a758dd492dbfd9ec4
tungchiahui <tungchia-hui@foxmail.com>
2023-02-28 11:19:34 +0800

    tung

--- Code1/test1.c
+++ Code1/test1.c
@@ -1,15 +1,15 @@
 //test.c
 #include <stdio.h>
 #include <stdlib.h>
+
+
+
+
 int main()
 {
-	char a = 0b11111101;
-//	char c = ((a << 6) >> 7);
-	char b = a << 6;
-	 b = b >> 7;
-	printf("%d",b);
+
+	int b = 10;
+	printf("我是%d",b);
 	system("pause");
 	return 0;
 }
-
-

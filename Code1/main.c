61a4528b11bbe47e5f9855f4df8aab778b26e27a
tungchiahui <tungchia-hui@foxmail.com>
2023-03-19 08:04:30 +0800

    tung

---
+++ Code1/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h> 
+
+
+int main()
+{
+
+    while(1)
+    {
+
+    }
+
+}
+
+
+
+
+
+
--- Code2/test2.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-    int a = 5;
-    int * b = (int *)a;
-
-
-}

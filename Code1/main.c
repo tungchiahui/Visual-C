2c29fe2c3b3dfa38f87d2b7abba1d37c104ef722
tungchiahui <tungchia-hui@foxmail.com>
2023-04-13 23:15:04 +0800

    tung

--- Code1/main.c
+++ Code1/main.c
@@ -1,18 +1,19 @@
-#include <stdio.h> 
+#include <stdio.h>
+#include <stdlib.h>
+struct  student_s
+{
+ char name[10];
+ int chinese;
+};
 
 
 int main()
 {
-
-    while(1)
-    {
-
-    }
+    struct student_s a;
+    a.chinese = 5;
+    printf("%d",a.chinese);
+    system("pause");
+    return 0;
 
 }
-
-
-
-
-
 
--- Code2/test1.c
+++ Code2/test1.c
@@ -1,35 +1,24 @@
 #include <stdio.h>
+#include <math.h>
 #include <stdlib.h>
-
-int Largest_common_divisor(int m, int n);//寻找最大公约数
-int Least_common_multiple(int m, int n);//寻找最小公倍数
 
 int main()
 {
-    int m, n;
-    scanf("%d %d", &m, &n);
-    printf("%d\n%d", Largest_common_divisor(m, n), Least_common_multiple(m, n));
+	int a[5], * p = a;
+	int i = 1;
+	while (i < 6)
+	{
+		printf("请输入第%d个数字:", i);
+		scanf_s("%d", &a);
+			i++;
+	}
+	
+	int j = 1;
+	while (j < 6) {
+		printf("第%d个数字为:%d\n", j, *p);
+		j++;
+		p++;
+	}
 	system("pause");
-    return 0;
-}
-
-
-int Largest_common_divisor(int m, int n)
-{
-    int small_num = m < n ? m : n;
-    for (int i = small_num; i > 0; i--)
-    {
-        if (m % i == 0 && n % i == 0)
-            return i;
-    }
-}
-
-int Least_common_multiple(int m, int n)
-{
-    int large_num = m > n ? m : n;
-    for (int i = large_num; ; i++)
-    {
-        if (i % m == 0 && i % n == 0)
-            return i;
-    }
+	return 0;
 }
